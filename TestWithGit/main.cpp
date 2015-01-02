67ee25bc1696c7b3561ed2b7f74add6666120959
lymtics0502 <congminh.entertainment@gmail.com>
2015-01-02 17:02:10 +0700

    Them chu thich

---
+++ TestWithGit/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main()
+{
+	int a, b;
+
+	printf("Input a: ");
+	scanf_s("%d", &a);
+	printf("Input b: ");
+	scanf_s("%d", &b);
+
+	printf("Sum of a and b is: %d\n", a + b);
+
+
+	// Them chu thich
+
+	return 0;
+}
